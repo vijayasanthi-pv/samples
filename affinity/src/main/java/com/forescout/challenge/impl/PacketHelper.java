58e004c690ba97cf9607861804da964b9c6313ac
vijayasanthi-pv <vijayasanthi.pv@gmail.com>
2021-03-30 19:44:11 +0400

    Add files via upload

--- affinity/src/main/java/com/forescout/challenge/impl/PacketHelper.java
+++ affinity/src/main/java/com/forescout/challenge/impl/PacketHelper.java
@@ -1,5 +1,7 @@
 package com.forescout.challenge.impl;
 
+import static com.forescout.challenge.affinity.Utility.cidrStringToNetAndMask;
+
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
@@ -15,8 +17,6 @@ import com.forescout.challenge.affinity.Utility;
 import com.forescout.challenge.affinity.attributes.Attribute;
 import com.forescout.challenge.affinity.attributes.values.Pair;
 
-import static com.forescout.challenge.affinity.Utility.cidrStringToNetAndMask;
-
 /**
  * Packet utility functions
  */
@@ -30,44 +30,15 @@ public class PacketHelper {
 	public static Map<IRoutingRule, Long> getSrcAddressCloseAffinity(IPacket packet, Collection<IRoutingRule> rules) {
 
 		Map<IRoutingRule, Object> srcAddressMap = rules.stream().collect(Collectors.toMap(Function.identity(),
-				routingRule -> ((Collection<Attribute<String>>) routingRule.getSrcAddresses()).stream().filter(srcAddress->Utility.isIpInsideNet(packet.getSrcAddr(), srcAddress.getArgument())).findFirst()));
+				routingRule -> ((Collection<Attribute<String>>) routingRule.getSrcAddresses().getArgument()).stream().filter(srcAddress->Utility.isIpInsideNet(packet.getSrcAddr(), srcAddress.getArgument())).findFirst().get()));
 
 		srcAddressMap.entrySet().stream().forEach(entry -> {
-			Collection<String> srcAddresses = (Collection) entry.getKey().getSrcAddresses().getArgument();
+			Collection<String> srcAddresses = (Set<String>)entry.getKey().getSrcAddresses().getArgument();
 			srcAddresses.clear();
-			srcAddresses.add((String) entry.getValue());
+			srcAddresses.add(entry.getValue().toString());
 		});
 		return getIPAddressCloseAffinity(srcAddressMap.keySet(), IRoutingRule.AttributeKey.srcAddresses);
 
-		/*Optional<Entry<IRoutingRule, Object>> optionalRule = srcAddressMap.entrySet().stream().max(Comparator.comparing(rule -> cidrStringToNetAndMask((String) rule.getValue()).getFirst()));
-
-		if (optionalRule.isPresent()) {
-			Entry<IRoutingRule, Object> rule = optionalRule.get();
-
-			Set<Entry<IRoutingRule, Object>> rulesSubnet = srcAddressMap.entrySet().stream().filter(item-> cidrStringToNetAndMask((String)item.getValue()).getFirst() ==
-					cidrStringToNetAndMask((String)rule.getValue()).getFirst())
-					.collect(Collectors.toSet());
-
-			if (rulesSubnet.size()==1)
-				return rulesSubnet.stream().findFirst().get().getKey();
-			else {
-				optionalRule = rulesSubnet.stream().max((rule1,rule2)-> cidrStringToNetAndMask((String)rule1.getValue()).getSecond()
-						.compareTo(cidrStringToNetAndMask((String)rule2.getValue()).getSecond()));
-				if (optionalRule.isPresent()) {
-					Entry<IRoutingRule, Object> ruleMask = optionalRule.get();
-
-					Set<Entry<IRoutingRule, Object>> rulesMask = rulesSubnet.stream().filter(item-> cidrStringToNetAndMask((String)item.getValue()).getSecond() ==
-							cidrStringToNetAndMask((String)ruleMask.getValue()).getSecond())
-							.collect(Collectors.toSet());
-
-					if (rulesMask.size()==1)
-						return rulesMask.stream().findFirst().get().getKey();
-					else
-						return rulesMask.stream().min((rule1,rule2)->((Integer)rule1.getKey().getId()).compareTo((Integer)(rule2.getKey().getId()))).get().getKey();
-				}
-			}
-		}
-		return null;*/
 	}
 
 		/**
@@ -84,18 +55,17 @@ public class PacketHelper {
 			IRoutingRule rule = optionalRule.get();
 			Long ruleIP = cidrStringToNetAndMask(getAttributeValue(rule, attributeKey)).getFirst();
 
-			Set<IRoutingRule> rulesSubnet = rules.stream().filter(item -> cidrStringToNetAndMask(getAttributeValue(item, attributeKey)).getFirst() == ruleIP).collect(Collectors.toSet());
+			Set<IRoutingRule> rulesSubnet = rules.stream().filter(item -> cidrStringToNetAndMask(getAttributeValue(item, attributeKey)).getFirst().equals(ruleIP)).collect(Collectors.toSet());
 
 			if (rulesSubnet.size() == 1) {
 				return Collections.singletonMap(rulesSubnet.stream().findFirst().get(), ruleIP);
 			} else {
-				// should this be Min / Max???
 				optionalRule = rulesSubnet.stream().min(Comparator.comparing(rule2 -> cidrStringToNetAndMask(getAttributeValue(rule2, attributeKey)).getSecond()));
 				if (optionalRule.isPresent()) {
 					IRoutingRule ruleMask = optionalRule.get();
 					Pair<Long, Integer> ruleIPMaskPair = cidrStringToNetAndMask(getAttributeValue(ruleMask, attributeKey));
 
-					Set<IRoutingRule> rulesMask = rulesSubnet.stream().filter(item-> cidrStringToNetAndMask(getAttributeValue(item, attributeKey)).getSecond() == ruleIPMaskPair.getSecond()).collect(Collectors.toSet());
+					Set<IRoutingRule> rulesMask = rulesSubnet.stream().filter(item-> cidrStringToNetAndMask(getAttributeValue(item, attributeKey)).getSecond().equals(ruleIPMaskPair.getSecond())).collect(Collectors.toSet());
 
 					if (rulesMask.size() == 1) {
 						return Collections.singletonMap(rulesMask.stream().findFirst().get(), ruleIPMaskPair.getFirst());
@@ -114,7 +84,7 @@ public class PacketHelper {
 		if(attributeKey == IRoutingRule.AttributeKey.dstAddress) {
 			return (String) rule.getDstAddress().getArgument();
 		} else if (attributeKey == IRoutingRule.AttributeKey.srcAddresses) {
-			return ((Collection<String>)rule.getSrcAddresses().getArgument()).stream().findFirst().get();
+			return Utility.ip2String(Long.parseLong((((Collection<String>)rule.getSrcAddresses()).stream().findFirst().get())));
 		}
 
 		return null;
